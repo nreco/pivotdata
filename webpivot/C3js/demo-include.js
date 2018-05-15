450b86db0f3edc2708d1b009c0d62a9020e18bca
Vitaliy Fedorchenko <fedorchenko@gmail.com>
2018-05-15 20:12:10 +0300

    Web pivot add-on for C3.js

--- webpivot/C3js/demo-include.js
+++ webpivot/C3js/demo-include.js
@@ -1,4 +1,5 @@
 /* loads dependencies for online demo */
 documentWriteScript("//cdnjs.cloudflare.com/ajax/libs/d3/4.13.0/d3.min.js");
 documentWriteScript("//cdnjs.cloudflare.com/ajax/libs/c3/0.6.0/c3.min.js");
+documentWriteStylesheet("//cdnjs.cloudflare.com/ajax/libs/c3/0.6.0/c3.min.css");
 documentWriteScript("https://cdn.rawgit.com/nreco/pivotdata/master/webpivot/C3js/jquery.nrecopivotchart-c3.js");
